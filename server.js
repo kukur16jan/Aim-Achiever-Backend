aa4e4f1a2362395ae76bdd8118c296f2483983c6
kukur16jan <charitsharma660@gmail.com>
2025-08-05 22:14:08 +0530

    new

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173', credentials: true}));
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}));
 app.use(session({
   secret: process.env.SESSION_SECRET || 'secret',
   resave: false,
